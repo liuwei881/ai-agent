7b616a75da7fdf63f5dc67e77ea049627d168a19
liuwei <liuwei@polex.com.cn>
2026-04-06 12:32:38 +0800

    增加attraction,tools,weather LLM py

---
+++ LLM.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Project  : ai-agent
+# File     : LLM
+# Author   : liuwei
+# Version  : v1.0
+# Email    : liuwei@genowis.com
+# Date     : 2026-04-05 09:57
+# Remarks  :
---
+++ tools.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+from weather import get_weather
+from attraction import get_attraction
+
+available_tools = {
+"get_weather": get_weather,
+"get_attraction": get_attraction,
+}
+
